a897265d9fab4bef8b41712a40c2fb3c7c12add9
Bajju00 <bajrangiyadavbhk@gmail.com>
2026-05-09 21:13:21 +0530

    updated ui

---
+++ src/Componenets/Down.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+const down = () => {
+  return (
+    <div className="bg-blue-200 h-screen w-full">
+      <h1>
+        Down
+      </h1>
+    </div>
+  )
+}
+
+export default down
